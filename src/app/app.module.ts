49bfe8b8e9c92f94fb52a4ccea2db488e4b5ea42
LukeSP23 <lukesp.2003@gmail.com>
2025-04-12 20:00:00 -0400

    Angular Forms Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddTaskComponent } from './add-task/add-task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddTaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
